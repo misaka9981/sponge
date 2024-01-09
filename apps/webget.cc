e05df881921ef839b72cd6b6778cfa59a0e7523b
misaka9981 <932353913@qq.com>
2024-01-09 00:11:34 +0000

    sock1->socket1

--- apps/webget.cc
+++ apps/webget.cc
@@ -20,7 +20,7 @@ void get_URL(const string &host, const string &path) {
     TCPSocket socket1;
     socket1.connect(Address(host, "http"));
     socket1.write("GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
-    while (!sock1.eof()) {
+    while (!socket1.eof()) {
         cout << socket1.read();
     }
     socket1.shutdown(SHUT_WR);
